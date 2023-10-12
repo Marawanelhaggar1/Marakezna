8b3b0efa6cc757f29239bc854770fae7fa7212df
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-10-12 05:32:22 +0300

    fixing log in bug

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\auth\loginRequest;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
@@ -17,11 +18,14 @@ class SocialiteController extends Controller
 
    public function handelGoogleCallback(){
 
-            $user=Socialite::driver('google')->stateless()->user();
-            $findUser = User::where('social_id', $user->id)->first();
-            if($findUser){
-                Auth::login($findUser);
-                return response()->json($findUser);
+       $user=Socialite::driver('google')->stateless()->user();
+       $findUser = User::where('social_id', $user->id)->first();
+       if($findUser){
+                return response()->json([
+                    'success' => true,
+                    'message' =>'Successfully logged in',
+                    'data' =>['token' => auth()->user()->createToken('auth_token')->plainTextToken,]
+                ]);
             }else{
                 $newUser = User::create([
                     'name' => $user->name,
@@ -29,7 +33,12 @@ class SocialiteController extends Controller
                     'social_id' => $user->id,
                     'password' => Hash::make('my-google')
                 ]);
-              return  Auth::login($newUser);
+
+                 return response()->json([
+                    'success' => true,
+                    'message' =>'Successfully logged in',
+                    'data' =>['token' => auth()->user()->createToken('auth_token')->plainTextToken,]
+                ]);
                 // return redirect('/home');
             }
 
